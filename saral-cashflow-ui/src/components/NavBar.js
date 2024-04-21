c1cbe0fc3867f4f86b15dd609ade146b68e36968
Sumit Jangir <sja@leapwork.com>
2024-04-21 19:28:57 +0530

    fixx

--- saral-cashflow-ui/src/components/NavBar.js
+++ saral-cashflow-ui/src/components/NavBar.js
@@ -1,10 +1,17 @@
 import { NavLink } from "react-router-dom";
+import Logo from "../imgs/icon_64.png";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark-subtle">
+    <nav className="navbar navbar-expand-lg bg-success">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand text-light" href="#">
+          <img
+            alt="logo"
+            className="me-2"
+            src={Logo}
+            style={{ height: "32px" }}
+          />
           Saral Cash Flow
         </a>
         <button
@@ -19,13 +26,26 @@ function NavBar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <NavLink to="/" className="nav-link" activeclassname="active">
+          <div className="navbar-nav me-auto mb-2 mb-lg-0">
+            <NavLink
+              to="/"
+              className="nav-link text-light"
+              activeclassname="active"
+            >
               Home
             </NavLink>
-            <NavLink to="/Master" className="nav-link" activeclassname="active">
+            <NavLink
+              to="/Master"
+              className="nav-link text-light"
+              activeclassname="active"
+            >
               Master
             </NavLink>
+          </div>
+          <div className="d-flex flex-row-reverse">
+            <button className="btn btn-success fw-bold" type="submit">
+              Logout
+            </button>
           </div>
         </div>
       </div>
