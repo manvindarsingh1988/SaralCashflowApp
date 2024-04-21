446718b16e7cbbcbf1e11a0a81e6d9d74c0d9463
Sumit Jangir <sja@leapwork.com>
2024-04-21 18:28:19 +0530

    Login Page

--- saral-cashflow-ui/src/App.js
+++ saral-cashflow-ui/src/App.js
@@ -6,20 +6,22 @@ import NavBar from "./components/NavBar";
 import Alert from "./components/alerts/alert";
 import AppContext from "./components/contexts/AppContext";
 import { showFadingAlert } from "./components/methods/utils";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <AppContext.Provider value={{ showFadingAlert: showFadingAlert }}>
-      <BrowserRouter>
-        <NavBar />
-        <Alert />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/Master" element={<Master />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
-  );
+  return <Login/>
+  // return (
+  //   <AppContext.Provider value={{ showFadingAlert: showFadingAlert }}>
+  //     <BrowserRouter>
+  //       <NavBar />
+  //       <Alert />
+  //       <Routes>
+  //         <Route exact path="/" element={<Home />}></Route>
+  //         <Route exact path="/Master" element={<Master />}></Route>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </AppContext.Provider>
+  // );
 }
 
 export default App;
